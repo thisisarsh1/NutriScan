1640bb21456b1daeebb65c41bed5579f80f6dc57
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-07-26 09:45:34 +0530

    Hero2 added

--- client/src/app/components/Signup.tsx
+++ client/src/app/components/Signup.tsx
@@ -8,6 +8,7 @@ import {
   IconBrandGoogle,
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
+import Link from "next/link";
 
 export function Signup() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -43,11 +44,11 @@ export function Signup() {
           <Input id="password" placeholder="••••••••" type="password" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your twitter password</Label>
+          <Label htmlFor="Confirmpassword">Your Confirm password</Label>
           <Input
-            id="twitterpassword"
+            id="Confirmpassword"
             placeholder="••••••••"
-            type="twitterpassword"
+            type="Confirmpassword"
           />
         </LabelInputContainer>
 
@@ -59,13 +60,14 @@ export function Signup() {
           <BottomGradient />
         </button>
         <br></br>
+        <Link href='/Login'>
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
           Login &rarr;
           <BottomGradient />
-        </button>
+        </button></Link>
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,18 @@
 import Hero from "./components/Hero";
+import {Hero2} from "./components/Hero2";
 
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03]">
+    <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03] overflow-x-hidden">
       <Hero></Hero>
+      <div className="relative">
+  <div className="absolute right-0 w-[50vw] h-[50vw] ">
+    <Hero2 />
+  </div>
+</div>
+
+      
     </div>
   );
 }
