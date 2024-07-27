53671daf700c6b78ce18f383b1d5fc0e6004d35d
Fareed <fareedsayed95@gmail.com>
2024-07-27 22:21:26 +0530

    bro authentication done completely

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,9 +1,15 @@
-from . models import FoodTesting_Scan
+from . models import FoodTesting_Scan_Image, FoodTesting_Scan_text
 from rest_framework import serializers
 
 
 
-class Food_Testing_Scan_serializer(serializers.ModelSerializer):
+class Food_Testing_Scan_Image_serializer(serializers.ModelSerializer):
     class Meta :
-        model = FoodTesting_Scan
+        model = FoodTesting_Scan_Image
+        fields = '__all__'
+
+
+class Food_Testing_Scan_text_serializer(serializers.ModelSerializer):
+    class Meta :
+        model = FoodTesting_Scan_text
         fields = '__all__'
