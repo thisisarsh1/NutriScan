8787ccff3f0b610b2459faa15e251fc70f7569e2
Fareed <fareedsayed95@gmail.com>
2024-07-30 15:30:51 +0530

    admin panel issue solved

--- server/api/models.py
+++ server/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin 
 from .managers import CustomUserManager
 # Create your models here.
 
@@ -19,16 +19,18 @@ class FoodTesting_Scan_text(models.Model):
     testing_id = models.AutoField(primary_key=True)
     scan_text = models.TextField()
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=500)
     username = None
     otp = models.CharField(max_length=6, blank=True, null=True)
     otp_expiration = models.DateTimeField(blank=True, null=True)
-    is_active = models.BooleanField(default=False)  # Make user inactive by default
+    is_active = models.BooleanField(default=True)  # Default to True for active users
+    is_staff = models.BooleanField(default=False)  # Add is_staff field if missing
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
