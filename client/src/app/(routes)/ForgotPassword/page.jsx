e28a69b4726c7559f69c18a2f20fa10af2554eaf
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-08-15 13:30:15 +0530

    Forgot Password Done

---
+++ client/src/app/(routes)/ForgotPassword/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ForgotPass from '@/app/components/ForgotPass'
+function page() {
+  return (
+    <div className='pt-[30vw] p-5 sm:pt-[10vw]'>
+      <ForgotPass></ForgotPass>
+    </div>
+  )
+}
+
+export default page
