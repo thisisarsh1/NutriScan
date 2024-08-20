a5d02546ea254a10ad5214005bf3250c59b8186a
Fareed <fareedsayed95@gmail.com>
2024-08-20 13:43:35 +0530

    the image loading problem sorted

--- server/user_profile/urls.py
+++ server/user_profile/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from . views import User_profile_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register('user_profile',User_profile_views, basename='user_profile')
 
 urlpatterns = [
     path('',include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
