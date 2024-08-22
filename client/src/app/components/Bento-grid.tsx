a62fc2e32d441f1458cc86a7a5d7729f11d7d1f2
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-08-22 21:24:42 +0530

    minor fixes

--- client/src/app/components/Bento-grid.tsx
+++ client/src/app/components/Bento-grid.tsx
@@ -1,149 +1,122 @@
 //@ts-nocheck
-"use client"
+"use client";
 import { cn } from "@/app/libs/utils";
 import React, { useEffect, useState } from "react";
 import { BentoGrid, BentoGridItem } from "@/app/components/ui/bento-grid";
-import BotResp from '@/app/components/userinfoComp/BotResp'
-import BarcodeResp from '@/app/components/userinfoComp/BarcodeResp'
-
-
+import BotResp from "@/app/components/userinfoComp/BotResp";
+import BarcodeResp from "@/app/components/userinfoComp/BarcodeResp";
 
 import { useToast } from "@/components/ui/use-toast";
-import { useUserContext } from '@/app/context/Userinfo';
+import { useUserContext } from "@/app/context/Userinfo";
 import {
-    IconClipboardCopy,
-    IconFileBroken,
-    IconSignature,
-    IconTableColumn,
-  } from "@tabler/icons-react";
-   
-  export function BentoGridDemo() {
-const { contextisLoggedIn,contextimg} = useUserContext(); // Updated hook
-const[info,setinfo]=useState('')
-    const { toast } = useToast();
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
 
-    const Getuserinfo = async () => {
-      const token = localStorage.getItem('authToken');
-      try {
-          const response = await fetch('http://127.0.0.1:8000/api/user', 
-          {
-              method: 'GET',
-              headers: {
-                "Authorization":token,
-                'Content-Type': "application/json",
-              },
-              credentials: 'include',
-            }
-            
-            );
-        if (!response.ok) {
-          
-          throw new Error('Failed to fetch user info'); // Handle error properly
-          
-        }
-        if (response.ok){
-          const result = await response.json();
-  
-       
-       
-        setinfo(result)
-        
+export function BentoGridDemo() {
+  const { contextisLoggedIn, contextimg } = useUserContext(); // Updated hook
+  const [info, setinfo] = useState("");
+  const { toast } = useToast();
+
+  const Getuserinfo = async () => {
+    const token = localStorage.getItem("authToken");
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/user", {
+        method: "GET",
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to fetch user info"); // Handle error properly
+      }
+      if (response.ok) {
+        const result = await response.json();
+
+        setinfo(result);
+
         // router.push("/")
-        }
-        
-      } catch (error) {
-        console.error("Error fetching user info:", error);
       }
-     
-    };
-    useEffect( ()=>{
-      if(contextisLoggedIn){
-        Getuserinfo()
-      }
-    },[])
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+    }
+  };
+  useEffect(() => {
+    if (contextisLoggedIn) {
+      Getuserinfo();
+    }
+  }, []);
 
-    const Skeleton = () => (
- 
-      <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
-    );
-    const Imgs = () => (
-
+  const Skeleton = () => (
+    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
+  );
+  const Imgs = () => (
     <img src="https://images.pexels.com/photos/4226256/pexels-photo-4226256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
-    );
-    const Profile = () => (
+  );
+  const Profile = () => <img src={contextimg} className=" h-52 mx-auto"></img>;
+  const items = [
+    {
+      title: "Bot Responses",
+      description: "Questions asked from BOT",
 
-      <img src={contextimg} className=" h-52 mx-auto"></img>
-      
-      );
-    const items = [
-      {
+      header: <BotResp />,
+      className: "md:col-span-2",
+      icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: `Edit Your Profile ${info.name}`,
+      description: info.email,
+      header: <Profile />,
 
-        title: "Bot Responses",
-        description: "Questions asked from BOT",
+      className: "md:col-span-1",
+      icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+      link: "/EditProfiles",
+    },
+    {
+      title: "Book A meeting With a Health Coatch",
+      description: "Get to know more about nutrition and excercise",
+      header: <Imgs />,
+      className: "md:col-span-1",
+      icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+      link: "/health-guides",
+    },
+    {
+      title: "The Barcode Responses !",
+      description: "Nutritional values of Food that You Scanned",
+      header: <BarcodeResp />,
+      className: "md:col-span-2",
+      icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    },
+  ];
+  return (
+    <div>
+      {contextisLoggedIn ? (
+        <BentoGrid className="py-[12%] mx-auto md:auto-rows-[20rem]">
+          {items.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              className={item.className}
+              icon={item.icon}
+              link={item.link}
+            />
+          ))}
+        </BentoGrid>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <p className="text-6xl lg:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-600 py-4 sm:py-8">
+            Please Login First
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
 
-        header: <BotResp />,
-        className: "md:col-span-2",
-        icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-      },
-      {
-
-        title: `Edit Your Profile ${info.name}`,
-        description: info.email,
-        header: <Profile/>,
-
-        className: "md:col-span-1",
-        icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-        link:"/EditProfiles"
-      },
-      {
-        title: "Book A meeting With a Health Coatch",
-        description: "Get to know more about nutrition and excercise",
-        header: <Imgs />,
-        className: "md:col-span-1",
-        icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-        link:"/health-guides"
-
-      },
-      {
-        title: "The Barcode Responses !",
-        description:
-          "Nutritional values of Food that You Scanned",
-        header: <BarcodeResp />,
-        className: "md:col-span-2",
-        icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-      },
-    ];
-return (
-  <div>
-    {contextisLoggedIn ? (
-      <BentoGrid className="py-[12%] mx-auto md:auto-rows-[20rem]">
-        {items.map((item, i) => (
-          <BentoGridItem
-            key={i}
-            title={item.title}
-            description={item.description}
-            header={item.header}
-            className={item.className}
-            icon={item.icon}
-            link ={item.link}
-          />
-        ))}
-      </BentoGrid>
-    ) : (
-      <div className="flex flex-col items-center justify-center h-screen">
-      <p className="text-6xl lg:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-600 py-4 sm:py-8">
-        Please Login First
-      </p>
-      </div>
-    )}
-  </div>
-);
-
-  }
-
-
-
-
-
-  
-export default BentoGridDemo
+export default BentoGridDemo;
--- client/src/app/components/ForgotPass.jsx
+++ client/src/app/components/ForgotPass.jsx
@@ -1,86 +1,75 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { cn } from "@/app/libs/utils";
 import { useToast } from "@/components/ui/use-toast";
-import { IconBrandGithub, IconBrandGoogle, IconBrandOnlyfans } from "@tabler/icons-react";
-import { useRouter } from 'next/navigation';
-import { useUserContext } from '@/app/context/Userinfo';
-import Link from "next/link";
+
+import { useRouter } from "next/navigation";
+
 import {
-    InputOTP,
-    InputOTPGroup,
-    InputOTPSeparator,
-    InputOTPSlot,
-  } from "@/components/ui/input-otp"
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
- function ForgotPass() {
-
+function ForgotPass() {
   const [new_password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const { contextsetIsLoggedIn,contextsetEmail,contextsetName} = useUserContext(); // Updated hook
-  const [loggedin,setLoggedin]=useState()
+
   const { toast } = useToast();
   const router = useRouter();
-const [confirm_password,setconfirm_password]=useState('')
-const [otp,setotp]=useState('')
-
-  // Update email context if it's empty
-
+  const [confirm_password, setconfirm_password] = useState("");
+  const [otp, setotp] = useState("");
 
   const handleSubmit = async () => {
-    console.log("setPass")
+    console.log("setPass");
     try {
-        const response = await fetch('http://127.0.0.1:8000/api/password-reset/', 
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/password-reset/",
         {
-            method: 'POST',
-            headers: {
-             
-              'Content-Type': "application/json",
-            },
-            body: JSON.stringify({ email,otp,new_password,confirm_password}),
-          }
-          
-          );
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, otp, new_password, confirm_password }),
+        }
+      );
       if (!response.ok) {
-        
-        throw new Error('Failed to Change Pasword'); // Handle error properly
-        
+        throw new Error("Failed to Change Pasword"); // Handle error properly
       }
-      if (response.ok){
+      if (response.ok) {
         const result = await response.json();
 
-    //   contextsetIsLoggedIn(true)
-    //   contextsetEmail(result.email)
-    //   contextsetName(result.name)
-      toast({
-        title: "Password Changed Successfully",
-       
-      });         
-      router.push("/Login")
+        //   contextsetIsLoggedIn(true)
+        //   contextsetEmail(result.email)
+        //   contextsetName(result.name)
+        toast({
+          title: "Password Changed Successfully",
+        });
+        router.push("/Login");
       }
-      
     } catch (error) {
       console.error("Error fetching user info:", error);
     }
-   
   };
-
-
 
   const sendOTP = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/password-reset-request/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        
-        body: JSON.stringify({ email }),
-      });
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/password-reset-request/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+
+          body: JSON.stringify({ email }),
+        }
+      );
 
       if (!response.ok) {
         toast({
@@ -91,13 +80,11 @@ const [otp,setotp]=useState('')
 
       const result = await response.json();
       if (response.ok) {
-
         toast({
-            title: "OTP successfully sent on your email ",
-          });
+          title: "OTP successfully sent on your email ",
+        });
         // localStorage.setItem('authToken', result.jwt);
-       
-  }
+      }
     } catch (error) {
       toast({
         title: "An error occurred",
@@ -106,12 +93,7 @@ const [otp,setotp]=useState('')
     }
 
     // Update loginInfo context after form submission
-    
   };
-
-
-  
-
 
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
@@ -119,13 +101,19 @@ const [otp,setotp]=useState('')
         Forgot Password ?
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-       Make a New One
+        Make a New One
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Input
+            id="email"
+            placeholder="projectmayhem@fc.com"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </LabelInputContainer>
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
@@ -137,44 +125,58 @@ const [otp,setotp]=useState('')
         <br></br>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="OTP">OTP</Label>
-          <div id="otp" placeholder="1234" type="otp" value={otp} onChange={(e) => setotp(e.target.value)} >
-          <InputOTP maxLength={6}  >
-          <InputOTPGroup >
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-          </InputOTPGroup>
-          <InputOTPSeparator />
-          <InputOTPGroup>
-            
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-          </InputOTPGroup>
-        </InputOTP>
-        </div>
+          <div
+            id="otp"
+            placeholder="1234"
+            type="otp"
+            value={otp}
+            onChange={(e) => setotp(e.target.value)}
+          >
+            <InputOTP maxLength={6}>
+              <InputOTPGroup>
+                <InputOTPSlot index={0} />
+                <InputOTPSlot index={1} />
+                <InputOTPSlot index={2} />
+              </InputOTPGroup>
+              <InputOTPSeparator />
+              <InputOTPGroup>
+                <InputOTPSlot index={3} />
+                <InputOTPSlot index={4} />
+                <InputOTPSlot index={5} />
+              </InputOTPGroup>
+            </InputOTP>
+          </div>
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" value={new_password} onChange={(e) => setPassword(e.target.value)} />
+          <Input
+            id="password"
+            placeholder="••••••••"
+            type="password"
+            value={new_password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">confirm_password</Label>
-          <Input id="confirm_password" placeholder="••••••••" type="password" value={confirm_password} onChange={(e) => setconfirm_password(e.target.value)} />
+          <Input
+            id="confirm_password"
+            placeholder="••••••••"
+            type="password"
+            value={confirm_password}
+            onChange={(e) => setconfirm_password(e.target.value)}
+          />
         </LabelInputContainer>
         <div className="flex flex-col space-y-4">
-        <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
-        >
-          SUBMIT &rarr;
-          <BottomGradient />
-        </button>
-       
+          <button
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+          >
+            SUBMIT &rarr;
+            <BottomGradient />
+          </button>
         </div>
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-        
       </form>
     </div>
   );
@@ -196,4 +198,4 @@ const LabelInputContainer = ({ children, className }) => {
     </div>
   );
 };
-export default ForgotPass
+export default ForgotPass;
