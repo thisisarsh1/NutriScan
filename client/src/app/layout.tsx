5d8f8f2bc917a791bcba87095bbec4a46f7347ff
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-09-02 18:33:47 +0530

    favicon 1

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -22,10 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="@/public/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="@/public/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="@/public/favicon-16x16.png" />
-        <link rel="manifest" href="@/public/site.webmanifest" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
       </head>
       <body className={inter.className}>
         <UserProvider>
