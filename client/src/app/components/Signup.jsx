2cc7622b8d412f5ec596f39a07fee5e592d675e5
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-09-29 20:53:18 +0530

    minor bug

--- client/src/app/components/Signup.jsx
+++ client/src/app/components/Signup.jsx
@@ -207,16 +207,7 @@ export function Signup() {
             </span>
             <BottomGradient />
           </button>
-          <button
-            className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="button"
-          >
-            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              OnlyFans
-            </span>
-            <BottomGradient />
-          </button>
+          
         </div>
       </form>
     </div>
